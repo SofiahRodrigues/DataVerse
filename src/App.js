a0001a1593a9c5825fba3f9252f7da72f3d02936
Sofiah Rodrigues <129235589+SofiahRodrigues@users.noreply.github.com>
2023-12-17 17:45:39 -0300

    Pilhas
    Atualização nas pilhas com pequenos ajustes, buscando deixá-la o mais automatizada possível. Colaboradores: Sofiah e Gean

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import CaixaDeTextoInterativa from './Components/pilhas';
-import './Components/pilhas.css';
-import Navbar from './Components/Navbar';
-import Footer from'./Components/Footer';
+import React from 'react';
+import Navbar from './Components/Navbar'; // Importe da Navbar 
+import './Components/Navbar.css'; 
+import Pilhas from './Components/pilhas'; // Importe o componente Pilhas
+import './App.css'; // Importe o arquivo CSS principal, ajuste conforme necessário
+import Footer from './Components/Footer'; // Importe do Footer
+import './Components/Footer.css';
+
 function App() {
   return (
     <div className="App">
-     <Navbar/>
-     
-     <CaixaDeTextoInterativa />
 
-     <Footer/>
+      <Navbar />
+      <h1>Estrutura de Pilhas</h1>
+      <a>
+        Pilhas, no contexto da ciência da computação, são estruturas de dados 
+        fundamentais que operam sob o princípio Last In, First Out (LIFO). Isso 
+        significa que o último elemento inserido é o primeiro a ser removido. 
+        Assim como uma pilha de pratos, onde adicionamos e retiramos sempre do topo, 
+        as pilhas são cruciais em muitos algoritmos e aplicativos. A operação "push"
+         adiciona um elemento ao topo, enquanto "pop" remove o elemento mais recente. 
+         Essa organização eficiente facilita o rastreamento de chamadas de funções, a 
+         avaliação de expressões matemáticas e a implementação de algoritmos, tornando 
+         as pilhas uma ferramenta valiosa na manipulação de dados temporários em programação.
+         </a>
+      <Pilhas />
+        
+      <Footer />
     </div>
   );
 }
--- src/Components/pilhas.js
+++ src/Components/pilhas.js
@@ -1,127 +1,105 @@
-import React from 'react';
-import './pilhas.css'; // Importe o arquivo CSS
+// pilhas.js
 
-class CaixaDeTextoInterativa extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      conteudo: 'Conteúdo inicial',
-      pilhaResultados: [],
-    };
-  }
+import React, { useState, useEffect } from 'react';
+import './pilhas.css'; // Importa estilos CSS
 
-  // Função para atualizar o conteúdo da caixa de texto
-  atualizarConteudo = (novoConteudo) => {
-    this.setState((prevState) => ({
-      conteudo: novoConteudo,
-      pilhaResultados: [...prevState.pilhaResultados, novoConteudo],
-    }));
-  };
+const Pilhas = () => {
+  // Estado para o conteúdo (não utilizado no código atual)
+  const [conteudo, setConteudo] = useState('Conteúdo inicial');
 
-  // Função para adicionar um item à pilha (push)
-  push = () => {
-    if (this.state.pilhaResultados.length < 10) {
-      const novoConteudo = this.state.pilhaResultados.length + 1;
-      this.setState((prevState) => ({
-        conteudo: novoConteudo,
-        pilhaResultados: [
-          ...prevState.pilhaResultados,
-          <div key={novoConteudo} className="numeroDestaque">{novoConteudo}</div>,
-        ],
-      }));
-    } else {
-      alert('Número máximo de caixas atingido (10).');
+  // Estado para a pilha e o número máximo de elementos permitidos
+  const [stack, setStack] = useState([]);
+  const maxStackSize = 10;
+
+  // Função para adicionar um novo elemento à pilha
+  const pushElement = () => {
+    if (stack.length < maxStackSize) {
+      const newElement = Math.floor(Math.random() * 100) + 1;
+      setStack((prevStack) => [...prevStack, newElement]);
     }
-    
   };
 
-
-  // Função para remover o último item da pilha (pop)
-  pop = () => {
-    if (this.state.pilhaResultados.length > 0) {
-      // Remove o último item da pilha
-      const [, ...novaPilhaResultados] = this.state.pilhaResultados;
-      this.setState({
-        pilhaResultados: novaPilhaResultados,
+  // Função para remover o elemento do topo da pilha
+  const popElement = () => {
+    if (stack.length > 0) {
+      setStack((prevStack) => {
+        const newStack = [...prevStack];
+        newStack.pop();
+        return newStack;
       });
-      this.atualizarConteudo(''); // Não exibe mensagem
     }
   };
 
-  top = () => {
-    if (this.state.pilhaResultados.length > 0) {
-      const topo = this.state.pilhaResultados[0].props.children;
-      alert(`Topo: ${topo}`);
-    } else {
-      alert('A pilha de resultados está vazia.');
-    }
-  };
-  
-  
-  
-  // Função para verificar se a pilha está vazia
-  isEmpty = () => {
-    const vazia = this.state.pilhaResultados.length === 0;
-    const mensagem = vazia ? 'A pilha de resultados está vazia.' : 'A pilha de resultados não está vazia.';
-    alert(mensagem);
-  };
-  
-
-// Função para obter o tamanho da pilha
-size = () => {
-  const numerosNaPilha = this.state.pilhaResultados.filter(item => !isNaN(item) && item !== '' && item !== null);
-  const tamanho = numerosNaPilha.length;
-  alert(`Tamanho da pilha: ${tamanho}`);
-};
-  
-  
-
-  // Função para limpar a caixa de texto
-  limparCaixaDeTexto = () => {
-    this.setState({
-      conteudo: 'Conteúdo inicial',
-      pilhaResultados: [],
-    });
+  // Função para obter o elemento no topo da pilha ou indicar que a pilha está vazia
+  const topElement = () => {
+    return stack.length > 0 ? stack[stack.length - 1] : 'Stack is empty';
   };
 
-  render() {
-    return (
-      <div>
-        {/* Exibe o conteúdo dinâmico com classes de estilo */}
-        <div className="caixaDeTexto">
-          {this.state.pilhaResultados.map((item, index) => (
-            <div key={index}>{item}</div>
-          ))}
+  // Função para verificar se a pilha está vazia ou cheia
+  const isEmpty = () => {
+    return stack.length > 0 ? 'Cheia' : 'Vazia';
+  };
+
+  // Função para obter o tamanho atual da pilha
+  const size = () => {
+    return stack.length;
+  };
+
+  // Efeito para simular a adição e remoção de elementos da pilha em intervalos de tempo
+  useEffect(() => {
+    const pushInterval = setInterval(() => {
+      pushElement();
+    }, 1500);
+
+    const popInterval = setInterval(() => {
+      popElement();
+    }, 3000);
+
+    // Limpa os intervalos quando o componente é desmontado ou quando a pilha atinge o tamanho máximo
+    return () => {
+      clearInterval(pushInterval);
+      clearInterval(popInterval);
+    };
+  }, [stack, maxStackSize]);
+
+  return (
+    <div>
+      {/* Caixa de texto para a pilha */}
+      <div className="caixaDeTexto">
+        <div className="stack-container">
+          {/* Exibe os elementos da pilha */}
+          <div className="stack">
+            {stack.map((element, index) => (
+              <div key={index} className="stack-element">
+                {element}
+              </div>
+            ))}
+          </div>
+          {/* Exibe informações formatadas sobre a pilha */}
+          <div className="info-container">
+            {/* Caixa colorida para o "Top" */}
+            <div className="info-box">Top: {topElement()}</div>
+            {/* Caixa colorida para o "IsEmpty" */}
+            <div className="info-box">IsEmpty: {isEmpty()}</div>
+            {/* Caixa colorida para o "Size" */}
+            <div className="info-box">Size: {size()}</div>
+          </div>
         </div>
+      </div>
 
-        {/* Botões para manipular a pilha */}
-        <button className="botaoAtualizar" onClick={this.push}>
+      {/* Botões na parte inferior esquerda com estilos personalizados */}
+      <div className="button-container-left">
+        {/* Botão para adicionar elemento à pilha */}
+        <button className="botaoPush" onClick={pushElement} disabled={stack.length >= maxStackSize}>
           Push
         </button>
-
-        <button className="botaoAtualizar2" onClick={this.pop}>
+        {/* Botão para remover elemento do topo da pilha */}
+        <button className="botaoPop" onClick={popElement} disabled={isEmpty() === 'Vazia'}>
           Pop
         </button>
-
-        <button className="botaoAtualizar3" onClick={this.top}>
-          Top
-        </button>
-
-        <button className="botaoAtualizar4" onClick={this.isEmpty}>
-          IsEmpty
-        </button>
-
-        <button className="botaoAtualizar5" onClick={this.size}>
-          Size
-        </button>
-
-        {/* Botão para limpar a caixa de texto */}
-        <button className="botaoLimpar" onClick={this.limparCaixaDeTexto}>
-          Limpar Informações
-        </button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-export default CaixaDeTextoInterativa;
+export default Pilhas;
